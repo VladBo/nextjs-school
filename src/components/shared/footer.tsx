4ec375656733afe9932d6eda90bc1fe0fe9d05f3
Vladyslav Bondarchuk <vladyslav_bondarchuk@epam.com>
2025-06-06 23:30:25 +0300

    feat: remove import through index.ts for header and footer

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -1,11 +1,11 @@
 import Link from 'next/link';
 
-export const Footer = () => {
+export function Footer() {
   return (
-    <footer className="flex w-full shrink-0 flex-col items-center gap-2 border-t px-4 py-6 sm:flex-row md:px-6">
+    <footer className="flex w-full shrink-0 flex-col items-center gap-2 border-t p-4 sm:flex-row md:px-6">
       <div className="container mx-auto flex flex-col items-center justify-between sm:flex-row">
         <p className="text-xs text-gray-500">
-          © 2025 Janna Bondarchuk. All rights reserved.
+          © {new Date().getFullYear()} Janna Bondarchuk. All rights reserved.
         </p>
         <nav className="mt-2 flex gap-4 sm:mt-0 sm:gap-6">
           <Link
@@ -24,4 +24,4 @@ export const Footer = () => {
       </div>
     </footer>
   );
-};
+}
